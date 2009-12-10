03f979b80fbb3b65eccd57b545b24f309d6d0968
mileszrobi <robert@milesz.hu>
2009-12-10 19:01:23 +0100

    packing consecutive duplicates problem started

---
+++ test/practice/PackConsecutiveDuplicatesIntoSublistTest.scala
@@ -0,0 +1,13 @@
+package practice
+
+import org.junit._
+
+class PackConsecutiveDuplicatesIntoSublistTest {
+	def verify(expected : List[Any], input : List[Any]) = {
+		Assert.assertEquals(expected, LearningLists.packConsecutiveDuplicatesIntoSublist(input))
+	} 
+	
+	@Test def nilMapsToNil = verify(Nil, Nil)
+	
+	@Test def listWithOneElementMapsToAListWithinTheList = verify(List(List(1)), List(1))
+}
