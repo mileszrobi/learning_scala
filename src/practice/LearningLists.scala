85231de06330d518622233d7f44c9490874f35f5
mileszrobi <robert@milesz.hu>
2010-01-13 19:52:21 +0100

    runlength decoding function done

--- src/practice/LearningLists.scala
+++ src/practice/LearningLists.scala
@@ -9,8 +9,12 @@ object LearningLists {
 	
 	def runLengthDecode [T](input : List[(Int, T)]) : List[T] = input match {
 		case Nil => Nil
-		case head :: tail => head._2 :: runLengthDecode(tail)
+		case (count, element) :: tail => repeat(element, count) ::: runLengthDecode(tail)
 	}
+	
+	def repeat[T](element : T, count : Int) : List[T] = 
+		if (count <= 0) Nil 
+		else element :: repeat(element, count - 1)
 	
 	def packConsecutiveDuplicatesIntoSublist[T](input : List[T]) : List[List[T]] = input match {
 		case Nil => Nil
