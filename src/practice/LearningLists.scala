7d0c99f35d107ff489070b190d108a0890ace9b5
mileszrobi <robert@milesz.hu>
2010-02-08 18:57:10 +0100

    P18 - two element list sliced after first element results in a list with the last element - refactor

--- src/practice/LearningLists.scala
+++ src/practice/LearningLists.scala
@@ -6,7 +6,7 @@ object LearningLists {
         input : List[T],
         left : Int,
         right : Int
-    ) = if (left == 0) List(input.head) else input.tail
+    ) = List(input(left))
     
     def split[T](firstLength : Int, input : List[T]) : Tuple2[List[T], List[T]] =
         if(firstLength <= 0) (Nil, input)
