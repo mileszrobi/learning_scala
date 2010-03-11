58471a6f143f861ea6fc45a7d8198a536464132f
mileszrobi <robert@milesz.hu>
2010-03-11 21:59:31 +0100

    Moved tests for the 'penultimate', 'nthFromLast' and 'isPalindrome' problems to Concordion.

--- test/practice/spec/ListsTest.scala
+++ test/practice/spec/ListsTest.scala
@@ -7,13 +7,18 @@ import org.concordion.integration.junit4.ConcordionRunner
 
 @RunWith(classOf[ConcordionRunner])
 class ListsTest {
+  implicit def csv2list(x: String) = if(x == "") Nil else List(x.split(" *, *") : _*)
 
-    implicit def csv2list(x: String) = if(x == "") Nil else List(x.split(",") : _*)
+  implicit def none2string[T](x : Option[T]) = x match {
+    case None => "None"
+    case Some(y) => y.toString
+  }
+  
+  def findLast (input : String) = none2string (LearningLists.lastOf(input))
 
-    implicit def none2string[T](x : Option[T]) = x match {
-        case None => "None"
-        case Some(y) => y.toString
-    }
+  def penultimate (input : String) = none2string (LearningLists.lastButOf(input))
 
-    def findLast (input : String) = none2string ( LearningLists.lastOf(input))
+  def nthFromLast (n : Int, input : String) = none2string (LearningLists.nthFromLast(n, input))
+
+  def isPalindrome (input : String) = LearningLists.isPalindrome(input)
 }
